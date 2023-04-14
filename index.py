217c7b1a0312d60df23a392d121017f337d9dcc7
Kefir4ik <zarya.yaroslav.pro@gmail.com>
2023-04-14 20:24:55 +0600

    test commit

---
+++ index.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from data import data
+from flask import Flask, render_template,request,redirect
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=data)
+
+@app.route('/add_card', methods=['POST'])
+def add_card() :
+    card = {}
+    card['id'] = datetime.datetime.now()
+    card['wifi'] = request.form['wifiname']
+    card['price'] = request.form['wifiprice']
+    card['speed'] = request.form['wifispeed']
+    data.append(card)
+    print(card)
+    return redirect('/')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', data=data)
+
+@app.route('/delete_card', methods=['POST'])
+def delete_card():
+    id = request.form['id']
+    for card in data:
+        print(str(id))
+        if str(card['id']) == str(id):
+            data.remove(card)
+    return redirect('/admin')
+
+if __name__ == '__main__':
+    app.run(debug=True)
